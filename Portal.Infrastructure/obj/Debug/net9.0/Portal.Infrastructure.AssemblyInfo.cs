a4e8fbb88aea05eb9ba6e663f64b1ef57d49bebf
Михаил Шийч <shiich@turovmilk.by>
2026-02-27 16:02:27 +0300

    Добавил logout для отключённого пользователя, если он уже авторизован.

--- Portal.Infrastructure/obj/Debug/net9.0/Portal.Infrastructure.AssemblyInfo.cs
+++ Portal.Infrastructure/obj/Debug/net9.0/Portal.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdf0d20594797111e4ec8028b5d34ee80e9490e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bd4e0699f954283ac972b2fdb43417b0b593e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
