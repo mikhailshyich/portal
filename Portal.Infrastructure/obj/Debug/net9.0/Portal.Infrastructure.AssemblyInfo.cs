f0508cc1ee65e2f8ad8d20a8485172edd058e3a0
Михаил Шийч <shiich@turovmilk.by>
2026-03-02 16:39:26 +0300

    Подправил пагинацию.

--- Portal.Infrastructure/obj/Debug/net9.0/Portal.Infrastructure.AssemblyInfo.cs
+++ Portal.Infrastructure/obj/Debug/net9.0/Portal.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e8fbb88aea05eb9ba6e663f64b1ef57d49bebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb03d08fecf8722a6d8264126d01d841877a9538")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
