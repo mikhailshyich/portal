fb03d08fecf8722a6d8264126d01d841877a9538
Михаил Шийч <shiich@turovmilk.by>
2026-03-02 15:11:40 +0300

    Добавил поиск по спискам. Чуть изменил добавление оборудования (визуально).

--- Portal.Domain/obj/Debug/net9.0/Portal.Domain.AssemblyInfo.cs
+++ Portal.Domain/obj/Debug/net9.0/Portal.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdf0d20594797111e4ec8028b5d34ee80e9490e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e8fbb88aea05eb9ba6e663f64b1ef57d49bebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Portal.Infrastructure/obj/Debug/net9.0/Portal.Infrastructure.AssemblyInfo.cs
+++ Portal.Infrastructure/obj/Debug/net9.0/Portal.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bd4e0699f954283ac972b2fdb43417b0b593e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e8fbb88aea05eb9ba6e663f64b1ef57d49bebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
